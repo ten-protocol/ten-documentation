011f1dd4c71aad26080ad6463b5afd6c9c6f9f0e
Gavin Thomas <gavin@obscu.ro>
2023-10-13 16:32:37 +0100

    Update docusaurus.config.js (#5)
    Updated baseUrl and production url to match docs.obscu.ro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://obscuronet.github.io/',
+  url: 'https://docs.obscu.ro/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/obscuro-documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
