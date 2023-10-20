f3082938f0ec142c0a513794a3b3c068de118352
Oswy <74738120+oswy-cpu@users.noreply.github.com>
2023-10-20 23:04:47 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/obscuronet/obscuro-documentation/blob/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/obscuronet/obscuro-documentation/blob/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
