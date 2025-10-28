4b73dfd4afbed1de54427dabc7a34016dc3f22ca
Will Hester <william_hester@me.com>
2025-10-28 07:15:34 +0000

    fix logo, remove stack overflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,60 +9,60 @@ const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'TEN Documentation',
-  tagline: 'Encrypting Ethereum',
-  favicon: 'img/favicon.ico',
+  title: "TEN Documentation",
+  tagline: "Encrypting Ethereum",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://docs.ten.xyz/',
+  url: "https://docs.ten.xyz/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ten-protocol', // Usually your GitHub org/user name.
-  projectName: 'ten-documentation', // Usually your repo name.
+  organizationName: "ten-protocol", // Usually your GitHub org/user name.
+  projectName: "ten-documentation", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   // Add Mendable configuration
   customFields: {
-    mendableAnonKey: process.env.MENDABLE_ANON_KEY || 'YOUR_ANON_KEY',
+    mendableAnonKey: process.env.MENDABLE_ANON_KEY || "YOUR_ANON_KEY",
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ten-protocol/ten-documentation/blob/main',
+            "https://github.com/ten-protocol/ten-documentation/blob/main",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ten-protocol/ten-documentation/blob/main'
+            "https://github.com/ten-protocol/ten-documentation/blob/main",
           //editUrl:
-            //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          //'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -70,91 +70,92 @@ const config = {
 
   plugins: [
     [
-      '@signalwire/docusaurus-plugin-llms-txt',
+      "@signalwire/docusaurus-plugin-llms-txt",
       {
-        siteTitle: 'TEN Documentation',
-        siteDescription: 'Comprehensive guide to TEN protocol',
+        siteTitle: "TEN Documentation",
+        siteDescription: "Comprehensive guide to TEN protocol",
         depth: 3,
         content: {
           includeBlog: false,
           includePages: true,
-          enableLlmsFullTxt: true  // Optional: generates llms-full.txt
-        }
-      }
-    ]
+          enableLlmsFullTxt: true, // Optional: generates llms-full.txt
+        },
+      },
+    ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
         logo: {
-          alt: 'TEN Logo',
-          src: 'img/logo.svg',
+          alt: "TEN Logo",
+          src: "img/logo.svg",
+          srcDark: "img/logo-white.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {to: 'https://medium.com/obscuro-labs', label: 'Blog', position: 'left'},
-          {
-            href: '/llms.txt',
-            label: 'llms.txt',
-            position: 'right',
-            target: '_blank',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Documentation",
           },
           {
-            href: '/llms-full.txt',
-            label: 'llms-full.txt',
-            position: 'right',
-            target: '_blank',
+            to: "https://medium.com/obscuro-labs",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            href: "/llms.txt",
+            label: "llms.txt",
+            position: "right",
+            target: "_blank",
+          },
+          {
+            href: "/llms-full.txt",
+            label: "llms-full.txt",
+            position: "right",
+            target: "_blank",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Introduction',
-                to: '/docs/category/introduction',
+                label: "Introduction",
+                to: "/docs/category/introduction",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/',
+                label: "Discord",
+                href: "https://discord.gg/Zgx2ycH7Xw",
               },
               {
-                label: 'Discord',
-                href: 'https://t.co/UJC0FUAY2T',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/tenprotocol',
+                label: "Twitter",
+                href: "https://twitter.com/tenprotocol",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: 'https://medium.com/obscuro-labs',
+                label: "Blog",
+                to: "https://medium.com/obscuro-labs",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/ten-protocol/ten-documentation',
+                label: "GitHub",
+                href: "https://github.com/ten-protocol/go-ten",
               },
             ],
           },
@@ -166,6 +167,6 @@ const config = {
         darkTheme: darkTheme,
       },
     }),
-};
+}
 
 module.exports = config;
